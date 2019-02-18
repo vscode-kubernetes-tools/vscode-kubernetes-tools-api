9885720d47b6d9303de995f3074db71559c5aace
Ivan Towlson <itowlson@microsoft.com>
2019-02-19 12:22:06 +1300

    Incantation to get the VS Code typings

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,11 @@
+import * as vscode from 'vscode';
+
 /**
  * Holds a place where there will one day be real APIs.
  *
  * @returns A placeholder
  */
 export function placeholder(): string {
+    console.log(vscode.extensions.all.length);
     return "placeholder";
 }
