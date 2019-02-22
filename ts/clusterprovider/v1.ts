eb79ad03850e2594f6e3b9097df89c0413fd909f
itowlson <github@hestia.cc>
2019-02-22 20:46:15 +1300

    More docs

--- ts/clusterprovider/v1.ts
+++ ts/clusterprovider/v1.ts
@@ -21,12 +21,54 @@ export interface ClusterProviderV1 {
 
 export namespace ClusterProviderV1 {
 
+    /**
+     * The actions for which a cluster provider may be invoked.
+     * The possible values are 'create' (the Create Cluster command)
+     * and 'configure' (the Add Existing Cluster command).
+     */
     export type ClusterProviderAction = 'create' | 'configure';
 
+    /**
+     * Represents a cluster provider - an object that provides a
+     * user interface for working with a particular type of Kubernetes
+     * cluster, such as Minikube or Azure Kubernetes Services.  The
+     * cluster provider should perform any requires interaction with the
+     * user, and take whatever platform-specific steps are required
+     * to add an existing cluster to the kubeconfig file or to create a
+     * cluster of the appropriate type, depending on the action for which
+     * it is invoked.
+     */
     export interface ClusterProvider {
+        /**
+         * A programmatic identifier for the cluster provider.
+         */
         readonly id: string;
+        /**
+         * A display mame for the cluster type. This is displayed in the
+         * cluster type drop-down at the start of the Add Existing Cluster
+         * or Create Cluster wizard.
+         */
         readonly displayName: string;
+        /**
+         * The actions supported by the cluster provider. For example, if
+         * you support registering existing clusters but not creating new
+         * clusters, this array should contain 'configure' only.
+         */
         readonly supportedActions: ClusterProviderAction[];
+        /**
+         * Invoked by the Kubernetes extension to perform actions and
+         * display user interface. This is first called when the user
+         * chooses your cluster type in the wizard; it is called again
+         * when your user interface transitions to a new page.
+         * @param wizard The wizard to use for showing any required user
+         * interface. Your implementation can call the showPage() method
+         * to prompt for information or display results.
+         * @param action The command the user invoked: 'configure' for
+         * Add Existing Cluster, 'create' for Create Cluster.
+         * @param message An object containing any information from the
+         * HTML form that requested to move to the next page.  TODO: provide
+         * more details about this.
+         */
         next(wizard: Wizard, action: ClusterProviderAction, message: any): void;
     }
 
