a2344d5361edb68ed433b63e25073b5e7faf30e9
Ivan Towlson <itowlson@microsoft.com>
2019-02-25 10:31:22 +1300

    JSDoc for the cluster provider version dispatcher

--- ts/clusterprovider/versions.ts
+++ ts/clusterprovider/versions.ts
@@ -2,8 +2,17 @@ import { APIKey, API } from '..';
 
 import { ClusterProviderV1 } from './v1';
 
+/**
+ * A key identifying v1 of the Kubernetes extension's Cluster Provider API.
+ */
 export const CLUSTER_PROVIDER_V1: APIKey<ClusterProviderV1> = { component: "clusterprovider", version: "v1" };
 
+/**
+ * Provides access to the Kubernetes extension's Cluster Provider API.
+ */
 export interface ClusterProviderAPI {
+    /**
+     * Provides access to v1 of the Kubernetes extension's Cluster Provider API.
+     */
     readonly v1: Promise<API<ClusterProviderV1>>;
 }
