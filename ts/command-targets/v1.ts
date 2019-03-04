8afc37376aeda8cc30ba5f109c42f4951a7c75e6
Ivan Towlson <itowlson@microsoft.com>
2019-03-05 12:16:03 +1300

    Updated explorer interfaces from extension

--- ts/command-targets/v1.ts
+++ ts/command-targets/v1.ts
@@ -1,17 +1,71 @@
+// This module is contractual and should not be changed after release.
+// It should be in sync with vscode-kubernetes-tools-api/ts/command-targets/v1.ts
+// at all times.
+
 export interface CommandTargetsV1 {
     resolve(target?: any): CommandTargetsV1.CommandTarget | undefined;
 }
 
 export namespace CommandTargetsV1 {
 
-    export interface KubernetesExplorerNode {
+    interface KubernetesExplorerNodeTarget {
         readonly targetType: 'kubernetes-explorer-node';
-        readonly id: string;
+        readonly node: KubernetesExplorerNode;
     }
 
-    export interface HelmExplorerNode {
+    export interface HelmExplorerNodeTarget {
         readonly targetType: 'helm-explorer-node';
     }
 
-    export type CommandTarget = KubernetesExplorerNode | HelmExplorerNode;
+    export interface KubernetesExplorerResourceNode {
+        readonly nodeType: 'resource';
+        readonly namespace: string | null;
+        readonly resourceKind: ResourceKind;
+        readonly name: string;
+        readonly metadata?: any;
+    }
+
+    export interface KubernetesExplorerGroupingFolderNode {
+        readonly nodeType: 'folder.grouping';
+    }
+
+    export interface KubernetesExplorerResourceFolderNode {
+        readonly nodeType: 'folder.resource';
+        readonly resourceKind: string;
+    }
+
+    export interface KubernetesExplorerContextNode {
+        readonly nodeType: 'context';
+        readonly name: string;
+    }
+
+    export interface KubernetesExplorerConfigDataItemNode {
+        readonly nodeType: 'configitem';
+        readonly name: string;
+    }
+
+    export interface KubernetesExplorerErrorNode {
+        readonly nodeType: 'error';
+    }
+
+    export interface KubernetesExplorerHelmReleaseNode {
+        readonly nodeType: 'helm.release';
+        readonly name: string;
+    }
+
+    export type KubernetesExplorerNode =
+        KubernetesExplorerResourceNode |
+        KubernetesExplorerGroupingFolderNode |
+        KubernetesExplorerResourceFolderNode |
+        KubernetesExplorerContextNode |
+        KubernetesExplorerConfigDataItemNode |
+        KubernetesExplorerErrorNode |
+        KubernetesExplorerHelmReleaseNode;
+
+    export type CommandTarget = KubernetesExplorerNodeTarget | HelmExplorerNodeTarget;
+
+    export interface ResourceKind {
+        readonly manifestKind: string;
+        readonly abbreviation: string;
+    }
 }
