8289c08c2eec01bf4c498ad1d954684411464e48
Ivan Towlson <itowlson@microsoft.com>
2019-03-18 11:15:04 +1300

    Update to unified cluster explorer API

---
+++ ts/cluster-explorer/v1.ts
@@ -0,0 +1,74 @@
+import * as vscode from 'vscode';
+
+export interface ClusterExplorerV1 {
+    resolveCommandTarget(target?: any): ClusterExplorerV1.ClusterExplorerNode | undefined;
+    registerNodeContributor(nodeContributor: ClusterExplorerV1.NodeContributor): void;
+    refresh(): void;
+}
+
+export namespace ClusterExplorerV1 {
+    export interface NodeContributor {
+        contributesChildren(parent: ClusterExplorerV1.ClusterExplorerNode | undefined): boolean;
+        getChildren(parent: ClusterExplorerV1.ClusterExplorerNode | undefined): Promise<Node[]>;
+    }
+
+    export interface Node {
+        getChildren(): Promise<Node[]>;
+        getTreeItem(): vscode.TreeItem;
+    }
+
+    export interface ClusterExplorerResourceNode {
+        readonly nodeType: 'resource';
+        readonly namespace: string | null;
+        readonly resourceKind: ResourceKind;
+        readonly name: string;
+        readonly metadata?: any;
+    }
+
+    export interface ClusterExplorerGroupingFolderNode {
+        readonly nodeType: 'folder.grouping';
+    }
+
+    export interface ClusterExplorerResourceFolderNode {
+        readonly nodeType: 'folder.resource';
+        readonly resourceKind: ResourceKind;
+    }
+
+    export interface ClusterExplorerContextNode {
+        readonly nodeType: 'context';
+        readonly name: string;
+    }
+
+    export interface ClusterExplorerConfigDataItemNode {
+        readonly nodeType: 'configitem';
+        readonly name: string;
+    }
+
+    export interface ClusterExplorerErrorNode {
+        readonly nodeType: 'error';
+    }
+
+    export interface ClusterExplorerHelmReleaseNode {
+        readonly nodeType: 'helm.release';
+        readonly name: string;
+    }
+
+    export interface ClusterExplorerExtensionNode {
+        readonly nodeType: 'extension';
+    }
+
+    export type ClusterExplorerNode =
+        ClusterExplorerResourceNode |
+        ClusterExplorerGroupingFolderNode |
+        ClusterExplorerResourceFolderNode |
+        ClusterExplorerContextNode |
+        ClusterExplorerConfigDataItemNode |
+        ClusterExplorerErrorNode |
+        ClusterExplorerHelmReleaseNode |
+        ClusterExplorerExtensionNode;
+
+    export interface ResourceKind {
+        readonly manifestKind: string;
+        readonly abbreviation: string;
+    }
+}
--- ts/cluster-explorer/versions.ts
+++ ts/cluster-explorer/versions.ts
@@ -1,13 +1,13 @@
 import { API } from '..';
 
-import { ExplorerTreeV1 } from './v1';
+import { ClusterExplorerV1 } from './v1';
 
 /**
  * Provides access to the Kubernetes extension's Explorer Tree API.
  */
-export interface ExplorerTreeAPI {
+export interface ClusterExplorerAPI {
     /**
      * Provides access to v1 of the Kubernetes extension's Explorer Tree API.
      */
-    readonly v1: Promise<API<ExplorerTreeV1>>;
+    readonly v1: Promise<API<ClusterExplorerV1>>;
 }
