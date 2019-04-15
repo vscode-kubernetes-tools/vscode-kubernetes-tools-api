4a85e3031b06f521d9f1f3e1677f48c7deb2b0cb
Ivan Towlson <itowlson@microsoft.com>
2019-04-15 13:43:31 +1200

    Interface for merge/save kubeconfig commands

--- ts/cloudexplorer/v1.ts
+++ ts/cloudexplorer/v1.ts
@@ -9,5 +9,8 @@ export namespace CloudExplorerV1 {
     export interface CloudProvider {
         readonly cloudName: string;
         readonly treeDataProvider: vscode.TreeDataProvider<any>;
+        getKubeconfigYaml(cluster: any): Promise<string | undefined>;
     }
+
+    export const SHOW_KUBECONFIG_COMMANDS_CONTEXT = "kubernetes.providesKubeconfig";
 }
